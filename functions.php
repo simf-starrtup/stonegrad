0372ac87f472ac5bf30e3660280eb10e29885060
startup-simf <service@startup-ws.ru>
2018-07-12 14:24:03 +0300

    add theme

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rcorp
+ * Date: 12.07.2018
+ * Time: 14:04
+ */
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_get_document_title(); ?></title>
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" />
+    <?php wp_head(); ?>
+</head>
+<body>
